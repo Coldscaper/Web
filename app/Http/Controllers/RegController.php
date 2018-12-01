383953fa1f0efd40181475eeb9505774a3296777
Coldscaper <coldscapex@gmail.com>
2018-12-01 21:13:34 +0300

    Add Reg Controller

---
+++ app/Http/Controllers/RegController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Пётр
+ * Date: 01.12.2018
+ * Time: 21:00
+ */
+
+namespace App\Http\Controllers;
+
+
+class RegController extends Controller
+{
+    public function index()
+    {
+        echo 'Reg';
+    }
+
+}
